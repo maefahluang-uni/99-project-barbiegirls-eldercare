5fa65d826a1043b43f7f748439aba7762fe49c97
nichamond143 <nichamond143@gmail.com>
2024-08-24 23:53:55 +0700

    added camera recovery and memory monitoring, deleted web app file

--- motion_detection_system/camera-check.py
+++ motion_detection_system/camera-check.py
@@ -2,21 +2,21 @@ import cv2
 
 
 def cam_test() -> None:
-    
+
     cap = cv2.VideoCapture(0)
-    if not cap.isOpened():  
+    if not cap.isOpened():
         print("failed to open cam")
     else:
-        print('cam opened on port {}'.format(0))
+        print("cam opened on port {}".format(0))
 
-        for i in range(10 ** 10):
+        for i in range(10**10):
             success, cv_frame = cap.read()
             if not success:
-                print('failed to capture frame on iter {}'.format(i))
+                print("failed to capture frame on iter {}".format(i))
                 break
-            cv2.imshow('Input', cv_frame)
+            cv2.imshow("Input", cv_frame)
             k = cv2.waitKey(1)
-            if k == ord('q'):
+            if k == ord("q"):
                 break
 
         cap.release()
@@ -24,5 +24,5 @@ def cam_test() -> None:
     return
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cam_test()
